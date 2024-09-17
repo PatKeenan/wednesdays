63e81e8af9f93b3c2e84b7138889c43e3ee70bcb
Pat Keenan <patkeenan.dev@gmail.com>
2024-09-17 16:08:26 -0400

    fix: alter the script for choosing the correct feature

--- apps/web-landing/app/page.tsx
+++ apps/web-landing/app/page.tsx
@@ -3,7 +3,7 @@
 import { LandingPage } from "@/components/landing-page";
 import { LandingPage2 } from "@/components/landing-page-2";
 
-import { PostHogFeature } from "posthog-js/react";
+import { useFeatureFlagVariantKey } from "posthog-js/react";
 
 // Used for A/B testing
 const experimentName = "landing-page-experiment";
@@ -13,22 +13,15 @@ const experimentVariants = {
 } as const;
 
 export default function Page() {
-  return (
-    <>
-      <PostHogFeature
-        flag={experimentName}
-        match={experimentVariants.Control}
-        fallback={<LandingPage />}
-      >
-        <LandingPage />
-      </PostHogFeature>
-      <PostHogFeature
-        flag={experimentName}
-        match={experimentVariants.Version2}
-        fallback={<LandingPage />}
-      >
-        <LandingPage2 />
-      </PostHogFeature>
-    </>
-  );
+  const variant = useFeatureFlagVariantKey(experimentName);
+
+  if (variant == experimentVariants.Control) {
+    return <LandingPage />;
+  }
+
+  if (variant == experimentVariants.Version2) {
+    return <LandingPage2 />;
+  }
+
+  return <LandingPage />;
 }
