f0b1d9a3b432367962c03d3b3d38cebc7e9e26c5
Pat Keenan <patkeenan.dev@gmail.com>
2024-09-18 22:41:22 -0400

    feat: add third landing page variation

---
+++ apps/web-landing/app/3/page.ts
@@ -0,0 +1,1 @@
+export { LandingPage3 as default } from "@/components/landing-page-3";
--- apps/web-landing/app/page.tsx
+++ apps/web-landing/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { LandingPage } from "@/components/landing-page";
 import { LandingPage2 } from "@/components/landing-page-2";
+import { LandingPage3 } from "@/components/landing-page-3";
 
 import { useFeatureFlagVariantKey } from "posthog-js/react";
 
@@ -10,6 +11,7 @@ const experimentName = "landing-page-experiment";
 const experimentVariants = {
   Control: "control",
   Version2: "version-2",
+  Version3: "version-3",
 } as const;
 
 export default function Page() {
@@ -21,6 +23,10 @@ export default function Page() {
 
   if (variant == experimentVariants.Version2) {
     return <LandingPage2 />;
+  }
+
+  if (variant == experimentVariants.Version3) {
+    return <LandingPage3 />;
   }
 
   return <LandingPage />;
